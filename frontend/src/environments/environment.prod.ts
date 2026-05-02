6d95efabf6efe3bb6f1983e4ef4d66cec47b2b18
Dhruv Dhola <dhruvdhola890@gmail.com>
2026-05-02 12:12:33 +0100

    final: complete project submission - README, report fixes, cleaned test events

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
-export const environment = {
+/*export const environment = {
   production: true,
   apiBaseUrl: 'http://localhost:3000'
+};
+*/
+
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://prediction-game-platform-production.up.railway.app'
 };
